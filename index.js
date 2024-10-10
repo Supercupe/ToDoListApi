e4052bf0d77b544b6875a7f032ae3563ce2db0af
Matyáš Hoza <matyashoza@Matyass-MacBook-Air.local>
2024-10-10 13:10:55 +0200

    Delete/get/post/pa tch

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 4000;
+
+let tasks = [
+   { id: 1,
+    content:"Buy groceries"
+}
+];
+
+let lastId = 1;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/tasks", (req, res)=>{
+    console.log(tasks)
+    res.json(tasks)
+})
+
+app.get("/tasks/:id", (req, res) => {
+    const task = tasks.find((t)=> t.id === parseInt(req.params.id));
+    if (!task) return res.status(404).json({message: "task not found"});
+    res.json(tasks)
+})
+
+app.post("/tasks", (req, res) => {
+    const newId = ++lastId;
+    const task = {
+        id: newId,
+        content: req.body.content,
+    };
+    tasks.push(task);
+    res.status(201).json(task);
+});
+
+app.patch("/tasks/:id", (req, res) => {
+    const task = tasks.find((t) => t.id === parseInt(req.params.id));
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    
+    if (req.body.content) {
+        task.content = req.body.content;
+    }
+    
+    res.json(task); 
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const index = tasks.findIndex((t) => t.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).json({ message: "Task not found" });
+
+    tasks.splice(index, 1);
+    res.json({ message: "Task deleted" });
+  });
+
+
+
+app.listen(port, () => {
+    console.log(`API is running at http://localhost:${port}`);
+  });
---
+++ server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const API_URL = "http://localhost:4000";
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}/tasks`);
+        console.log(response.data);  
+        res.render("index.ejs", { tasks: response.data }); 
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: "Error fetching tasks" });
+    }
+});
+
+app.get("/new", (req, res) => {
+    res.render("modify.ejs", { heading: "New Task", submit: "Create Task" });
+});
+
+app.get("/edit/:id", async (req, res) => {
+    try {
+      const response = await axios.get(`${API_URL}/tasks/${req.params.id}`);
+      console.log(response.data);
+      res.render("modify.ejs", {
+        heading: "Edit Task",
+        submit: "Update Task",
+        task: response.data,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching post" });
+    }
+  });
+
+  app.post("/api/tasks", async (req, res) => {
+    try {
+        const response = await axios.post(`${API_URL}/tasks`, req.body);
+        console.log(response.data);
+        res.redirect("/");
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: "Error creating task" });
+    }
+});
+
+app.post("/api/tasks/:id", async (req, res) => {
+    console.log("called");
+    try {
+      const response = await axios.patch(
+        `${API_URL}/tasks/${req.params.id}`,
+        req.body
+      );
+      console.log(response.data);
+      res.redirect("/");
+
+    } catch (error) {
+      res.status(500).json({ message: "Error updating post" });
+    }
+  });
+
+app.get("/api/tasks/delete/:id", async (req, res) => {
+    try {
+      await axios.delete(`${API_URL}/tasks/${req.params.id}`);
+      res.redirect("/");
+    } catch (error) {
+      res.status(500).json({ message: "Error deleting post" });
+    }
+  });
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000!");
+});
